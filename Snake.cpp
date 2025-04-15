77c9db968ed1f088fe72f273d750a8c769c2926e
UIT24730120 <24730120@ms.uit.edu.vn>
2025-04-15 15:44:03 +0700

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -1,1 +1,6 @@
-Loc nInh
+#include <iostream>
+#include <windows.h> // Cho gotoxy, Sleep, COORD, GetStdHandle, SetConsoleCursorPosition
+#include <cstdlib>   // Cho rand, srand, system
+#include <conio.h>   // Cho kbhit, getch
+#include <ctime>     // Cho time (để khởi tạo bộ sinh số ngẫu nhiên)
+#include <vector>    // Sử dụng vector thay cho mảng cố định sẽ linh hoạt hơn, nhưng ta sẽ giữ nguyên
